6918687ce5513e946b25790d9e729f91833265d6
Alejandro <42217598+dazm1710@users.noreply.github.com>
2018-08-29 07:57:13 -0500

    Add files via upload

---
+++ CreacionPersonajes/src/Interfaz/Fuentes.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Interfaz;
+
+import java.awt.Font;
+import java.io.InputStream;
+
+/**
+ *
+ * @author chris
+ */
+public class Fuentes {
+    private Font font = null;
+    public String TRON = "TRON.TTF";    
+
+    /* Font.PLAIN = 0 , Font.BOLD = 1 , Font.ITALIC = 2
+ * tamanio = float
+ */
+    public Font fuente( String fontName, int estilo, float tamanio)
+    {
+         try {
+            //Se carga la fuente
+            InputStream is =  getClass().getResourceAsStream(fontName);
+            font = Font.createFont(Font.TRUETYPE_FONT, is);
+        } catch (Exception ex) {
+            //Si existe un error se carga fuente por defecto ARIAL
+            System.err.println(fontName + " No se cargo la fuente");
+            font = new Font("Arial", Font.PLAIN, 14);            
+        }
+        Font tfont = font.deriveFont(estilo, tamanio);
+        return tfont;
+    }
+}
